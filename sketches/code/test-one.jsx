d4c8a83d8ec4a532943399317f6aacdcfdaf6708
Christian Selig <cselig@uw.edu>
2021-03-01 10:50:49 -0800

    remove test files

--- sketches/code/test-one.jsx
+++
@@ -1,5 +0,0 @@
-import styles from '../../styles/sketches/test-one.module.scss'
-
-export default function TestOne() {
-  return <p className={styles.heavy}>test one component</p>
-}
--- sketches/code/test-two.jsx
+++
@@ -1,5 +0,0 @@
-import styles from '../../styles/sketches/test-two.module.scss'
-
-export default function TestTwo() {
-  return <p className={styles.underline}>test two component</p>
-}
