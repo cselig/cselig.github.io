9bb5965c697ecfabee1df007cc12a9b679160423
Christian Selig <cselig@uw.edu>
2021-03-14 01:34:59 -0800

    tweak some text

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,19 +13,22 @@ function About() {
           My name is Christian Selig and I'm a software engineer based in the Bay Area.
         </p>
         <p>
-          I currently work for Cisco on <a href="https://www.cisco.com/c/dam/en/us/solutions/collaboration/people-insights-whitepaper.pdf">People Insights</a>
-          {' '}using technologies such as Rails, Postgres, Redis, C++, React, and ClojureScript.
+          I currently work for Cisco on <a href="https://www.cisco.com/c/dam/en/us/solutions/collaboration/people-insights-whitepaper.pdf">People Insights</a>.{' '}
+          I do end-to-end feature development including front end (React, ClojureScript) and backend (Rails, Postgres) work,{' '}
+          as well as more systems-oriented work in C++.
         </p>
         <p>
-          Previously I worked for a data science consultancy called Cascade Data Labs doing data analysis, engineering, and modeling using technologies such as
-          Python, SQL, Spark, Tableau, and Airflow.
+          Previously I worked for a data science consultancy called Cascade Data Labs doing data analysis, engineering, and modeling.
         </p>
         <p>
           I've done a little bit of open source development for a Python data quality library
           called Great Expectations - you can read about my contributions <a href="https://greatexpectations.io/blog/great-expectations-now-supports-executions-in-spark-a-blog-with-much-clapping/">here</a>.
         </p>
         <p>
-          I'm also a very active cellist and I enjoy playing orchestral and chamber music (and attending summer music festivals
+          I have a Bachelors degree in Chemical Engineering from the University of Washington.
+        </p>
+        <p>
+          Finally, I'm a very active cellist and I love playing orchestral and chamber music (and attending summer music festivals
           when I get the chance).
         </p>
       </div>
