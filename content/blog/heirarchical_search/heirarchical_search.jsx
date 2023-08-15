4ad04dd9df0bed5594f38a76d85bc68cdb67ffc2
cselig <seligchristian@gmail.com>
2023-08-14 23:21:41 -0700

    Add job list

--- content/blog/heirarchical_search/heirarchical_search.jsx
+++ content/blog/heirarchical_search/heirarchical_search.jsx
@@ -18,6 +18,10 @@ const LOCATIONS = [
         ]
       },
       {
+        value: 'Washington',
+        children: [{value: 'Seattle'}],
+      },
+      {
         value: 'Remote'
       },
     ]
@@ -67,15 +71,91 @@ const JOB_CATEGORIES = [
   },
   {
     value: 'Marketing',
-    children: [{value: 'SEO Specialist'}, {value: 'Copywriter'}, {value: 'Social Media'}]
+    children: [{value: 'SEO'}, {value: 'Copywriting'}, {value: 'Social Media'}]
   }
 ]
+
+const LOREM_IPSUM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Sed ligula elit, dapibus a justo sed, finibus ornare metus.
+Class aptent taciti sociosqu ad litora torquent per conubia
+nostra, per inceptos himenaeos. Nunc consequat aliquam pulvinar.
+Quisque eu ex mi. Sed suscipit nisi ac imperdiet scelerisque.
+Sed facilisis mauris purus, ultrices dignissim ex facilisis quis.
+Aenean vel lectus nunc`.split(".");
+
+const generateIpsum = () => {
+  const cutPoint = 0.5 + Math.random() / 2;
+  const sentances = Math.ceil(cutPoint * LOREM_IPSUM.length);
+  return LOREM_IPSUM.slice(0, sentances).join(". ") + ".";
+}
+
+const JOBS = [
+  {
+    title: 'Senior Software Engineer',
+    location: ['US', 'Remote'],
+    category: ['Software Engineering', 'Backend'],
+  },
+  {
+    title: 'UX Engineer, Payments',
+    location: ['Europe', 'Germany', 'Berlin'],
+    category: ['User Experience', 'Eng'],
+  },
+  {
+    title: 'Lead Copywriter',
+    location: ['US', 'California', 'San Diego'],
+    category: ['Marketing', 'Copywriting']
+  },
+  {
+    title: 'UX Research Program Manager',
+    location: ['US', 'Washington', 'Seattle'],
+    category: ['User Experience', 'Research'],
+  },
+  {
+    title: 'UX Researcher (new grad)',
+    location: ['US', 'Washington', 'Seattle'],
+    category: ['User Experience', 'Research'],
+  },
+  {
+    title: 'UX Researcher II',
+    location: ['US', 'Washington', 'Seattle'],
+    category: ['User Experience', 'Research'],
+  },
+  {
+    title: 'Software Engineer II, Frontend',
+    location: ['US', 'California', 'San Francisco'],
+    category: ['Software Engineering', 'Frontend'],
+  },
+  {
+    title: 'Infrastructure Software Engineer',
+    location: ['US', 'California', 'San Francisco'],
+    category: ['Software Engineering', 'Infra'],
+  },
+  {
+    title: 'Site Reliability Engineer',
+    location: ['Europe', 'Germany', 'Hamburg'],
+    category: ['Software Engineering', 'SRE'],
+  },
+  {
+    title: 'Senior Site Reliability Engineer',
+    location: ['Europe', 'Germany', 'Hamburg'],
+    category: ['Software Engineering', 'SRE'],
+  },
+  {
+    title: 'Social Media Manager',
+    location: ['Europe', 'UK', 'London'],
+    category: ['Marketing', 'Social Media'],
+  },
+]
+
+for (let job of JOBS) {
+  job.description = generateIpsum();
+}
 
 function Chip({value, index, remove, color}) {
   return (
     <div className="chip" style={{zIndex: 10 - index, backgroundColor: color}}>
       <p>{value}{' '}
-        <span className="remove" onClick={() => remove(value)}>x</span>
+        {remove && <span className="remove" onClick={() => remove(value)}>x</span>}
       </p>
     </div>
   )
@@ -92,8 +172,7 @@ function Options({options, select}) {
   )
 }
 
-function Chips({vals, colors}) {
-  const [path, setPath] = useState([])
+function Chips({path, setPath, vals, colors}) {
   const [options, setOptions] = useState(vals)
 
   const select = (node) => {
@@ -117,7 +196,7 @@ function Chips({vals, colors}) {
     return <Chip key={value} value={value} remove={remove} index={i} color={colors[i]} />
   })
   return (
-    <div className="filter">
+    <div className="filter-chip">
       <div className="chips">
         {chips}
       </div>
@@ -126,15 +205,64 @@ function Chips({vals, colors}) {
   );
 }
 
+function ValueChips({path, colors}) {
+  const chips = path.map((value, i) => {
+    return <Chip key={value} value={value} index={i} color={colors[i]} />
+  });
+  return (
+    <div className="value-chip">
+      <div className="chips">
+        {chips}
+      </div>
+    </div>
+  );
+}
+
+const objEqual = (obj1, obj2) => JSON.stringify(obj1) === JSON.stringify(obj2);
+
+function JobList({locationFilter, categoryFilter}) {
+  const jobId = (job) => job.title + JSON.stringify(job.location) + JSON.stringify(job.category);
+
+  const jobHtml = (job) => (
+    <div key={jobId(job)} className="job-card">
+      <h3 className="title">{job.title}</h3>
+      <p className="description">{job.description}</p>
+      <div className="value-chips">
+        <ValueChips path={job.location} colors={COLOR_PALLETES[0]} />
+        <ValueChips path={job.category} colors={COLOR_PALLETES[1]} />
+      </div>
+    </div>
+  );
+
+  const filteredJobs = JOBS.filter(job => {
+    return objEqual(locationFilter, job.location.slice(0, locationFilter.length)) &&
+      objEqual(categoryFilter, job.category.slice(0, categoryFilter.length));
+  }).map(job => jobHtml(job));
+
+  if (filteredJobs.length === 0) {
+    return <div className="job-list empty">
+      <p>No jobs found! Please broaden your filters.</p>
+    </div>
+  }
+
+  return <div className="job-list">{filteredJobs}</div>;
+}
+
 function Component() {
+  const [location, setLocation] = useState([]);
+  const [category, setCategory] = useState([]);
+
   return (
     <div id="categories">
       <div className="filters">
         <h3>Location</h3>
-        <Chips vals={LOCATIONS} colors={COLOR_PALLETES[0]} />
+        <Chips path={location} setPath={setLocation}
+          vals={LOCATIONS} colors={COLOR_PALLETES[0]} />
         <h3>Job Type</h3>
-        <Chips vals={JOB_CATEGORIES} colors={COLOR_PALLETES[1]} />
+        <Chips path={category} setPath={setCategory}
+          vals={JOB_CATEGORIES} colors={COLOR_PALLETES[1]} />
       </div>
+      <JobList locationFilter={location} categoryFilter={category} />
     </div>
   );
 }
