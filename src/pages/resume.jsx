4283d8caf105e1894fe767e76628ede70392af06
cselig <seligchristian@gmail.com>
2024-04-11 17:27:00 -0700

    resume updates

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -19,11 +19,30 @@ function Resume() {
         <div className="list">
           <div className="experience">
             <div className="body">
+              <h3>Google <span className="position">Software Engineer</span>
+              </h3>
+              <ul>
+                <li>Worked in the interconnect group of gChips, which designs SoC's for Pixel devices</li>
+                <li>Learned hardware concepts quickly to become an effective contributor</li>
+                <li>Built a Python placement engine for structured standard-cell placements. Used this engine
+                  on several modules to produce double-digit power savings as compared to standard PnR tools
+                </li>
+              </ul>
+            </div>
+
+            <div className="info">
+              <p>Aug 2022 - Present</p>
+              <p>Mountain View, CA</p>
+            </div>
+          </div>
+
+          <div className="experience">
+            <div className="body">
               <h3>Manara <span className="position">Software Engineer</span>
               </h3>
               <ul>
                 <li>Completed a contract to build a redesigned marketing website for a YC-backed startup</li>
-                <li><b>Technologies used:</b> React, NextJS, TailwindCSS</li>
+                {/* <li><b>Technologies used:</b> React, NextJS, TailwindCSS</li> */}
               </ul>
             </div>
 
@@ -37,11 +56,11 @@ function Resume() {
             <div className="body">
               <h3>Cisco Systems <span className="position">Software Engineer</span></h3>
               <ul>
-                <li>Integrated acquired startup's "digital chief of staff" product into Webex</li>
+                {/* <li>Integrated acquired startup's "digital chief of staff" product into Webex</li> */}
                 <li>Rewrote a large C++ data serialization system, improving maintainability and code velocity</li>
-                <li>Built internal tools for managing news feeds to make moderation more efficient</li>
+                <li>Worked directly with internal users to build tools for efficient moderation of news feeds</li>
                 <li>Responded to and debugged issues in production using application and system logs</li>
-                <li><b>Technologies used:</b> Ruby, C++, ClojureScript, React, Rails, PostgreSQL, Redis, D3.js</li>
+                {/* <li><b>Technologies used:</b> Ruby, C++, ClojureScript, React, Rails, PostgreSQL, Redis, D3.js</li> */}
               </ul>
             </div>
 
@@ -55,10 +74,10 @@ function Resume() {
             <div className="body">
               <h3>Cascade Data Labs <span className="position">Data Scientist</span></h3>
               <ul>
-                <li>Worked with clients to identify, scope, and implement projects unlocking data analysis</li>
+                {/* <li>Worked with clients to identify, scope, and implement projects unlocking data analysis</li> */}
                 <li>Contributed to writing libraries for data pipeline creation and monitoring</li>
-                <li>Integrated data quality monitoring into a high-visibility data pipeline</li>
-                <li><b>Technologies used:</b> Python, SQL, Spark, Airflow, Tableau</li>
+                <li>Designed and implemented a system for data quality monitoring that was able to catch several major data bugs before stakeholders</li>
+                {/* <li><b>Technologies used:</b> Python, SQL, Spark, Airflow, Tableau</li> */}
               </ul>
             </div>
             <div className="info">
@@ -77,10 +96,13 @@ function Resume() {
           <div className="experience">
             <div className="body">
               <h3>Manara <span className="position">Volunteer</span></h3>
-              <ul>
-                <li>Gave 1-3 mock technical interviews per week to trainees</li>
-                <li>TA'd for a coding interview prep class</li>
-              </ul>
+              {/* <ul>
+                <li>Manara is a </li>
+                <li>Have consistently received great feedback from interviewees.</li>
+              </ul> */}
+              Manara is an organization that helps train and place underemployed tech talent in the Middle East.
+              I volunteer by giving mock coding interviews. I find this work very rewarding and have been
+              consistently been given great feedback from my interviewees on the quality of my interviews.
             </div>
             <div className="info">
               <p>Nov 2021 – Present</p>
@@ -91,14 +113,21 @@ function Resume() {
             <div className="body">
               <h3>Great Expectations <span className="position">Open Source Contributor</span></h3>
               <ul>
-                <li>Built a PySpark back-end to enable working with large datasets</li>
-                <li>Designed and implemented a caching layer to improve performance</li>
-                <li>Proactively worked with library creators to pitch these features and get approval for designs</li>
-                <li>Please see this{' '}
+                {/* <li>Built a PySpark back-end to enable working with large datasets</li> */}
+                {/* <li>Designed and implemented a caching layer to improve performance</li> */}
+                {/* <li>Proactively worked with library creators to pitch these features and get approval for designs</li> */}
+                {/* <li>Please see this{' '}
                   <a href="https://greatexpectations.io/blog/great-expectations-now-supports-executions-in-spark-a-blog-with-much-clapping/">article</a>
                   {' '}highlighting my contributions
-                </li>
+                </li> */}
               </ul>
+              <p>
+                Great Expectations is an OS library for data quality assertions. During my time at Cascade Data Labs
+                I implemented a PySpark backend so we could integrate GE into our stack. I also completed a major
+                refactor of the codebase and introduced a metrics caching layer for performance.
+                Please see this <a href="https://greatexpectations.io/blog/great-expectations-now-supports-executions-in-spark-a-blog-with-much-clapping/">article</a>
+                {' '}for more details.
+              </p>
             </div>
             <div className="info">
               <p>2019</p>
@@ -120,9 +149,9 @@ function Resume() {
 
           <h4>Computer Science Coursework:</h4>
           <ul>
-            <li>Compilers <span className="detail">Stanford CS 143 through edX</span></li>
-            <li>Data Structures and Algorithms <span className="detail">UW CSE 373</span></li>
-            <li>Introduction to AI <span className="detail">UW CSE 415</span></li>
+            <li>Compilers <span className="detail">Stanford CS 143 through edX, 2022</span></li>
+            <li>Introduction to AI <span className="detail">UW CSE 415, 2017</span></li>
+            <li>Data Structures and Algorithms <span className="detail">UW CSE 373, 2016</span></li>
           </ul>
 
         </div>
